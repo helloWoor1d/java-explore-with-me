d38439bea2611453743fb8e556053026a06bc6e6
mira <pavuuuuuk@yandex.ru>
2025-03-09 04:50:00 +0300

    fix: исправления в docker compose для настройки взаимодействия основного сервиса и сервиса статистики

--- ewm-service/src/main/java/ru/practicum/ewm/configuration/StatsClientConfig.java
+++ ewm-service/src/main/java/ru/practicum/ewm/configuration/StatsClientConfig.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,15 @@ import ru.practicum.client.StatsClient;
 @Configuration
 public class StatsClientConfig {
     @Bean
-    public StatsClient statsClient() {
-        return new StatsClient(
-                WebClient.builder().baseUrl("http://localhost:9090").build());
+    public StatsClient statsClient(WebClient webClient) {
+        return new StatsClient(webClient);
+    }
+
+    @Bean
+    public WebClient webClient(@Value("${stats-service.url}") String url) {
+        System.out.println("Connecting to Stats Service at: " + url);
+        return WebClient.builder()
+                .baseUrl(url)
+                .build();
     }
 }
--- ewm-stats/stats-service/src/main/java/ru/practicum/stats/controller/StatsController.java
+++ ewm-stats/stats-service/src/main/java/ru/practicum/stats/controller/StatsController.java
@@ -1,7 +1,9 @@
 package ru.practicum.stats.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.stats.model.EndpointHitDto;
-import ru.practicum.stats.model.ViewStatsDto;
 import ru.practicum.stats.mapper.HitMapper;
 import ru.practicum.stats.model.EndpointHit;
+import ru.practicum.stats.model.EndpointHitDto;
 import ru.practicum.stats.model.ViewStats;
+import ru.practicum.stats.model.ViewStatsDto;
 import ru.practicum.stats.service.StatsService;
 
 import java.net.URLDecoder;
@@ -28,6 +30,7 @@ import static ru.practicum.stats.model.DateTimeFormat.DATE_TIME_FORMATTER;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@Slf4j
 public class StatsController {
     private final HitMapper mapper;
     private final StatsService service;
@@ -35,6 +38,7 @@ public class StatsController {
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
     public void saveHit(@Valid @RequestBody EndpointHitDto dto) {
+        log.debug("controller: запрос на добавление статистики stats {}", dto);
         EndpointHit hit = mapper.fromDto(dto);
         service.saveHit(hit);
     }
@@ -44,10 +48,15 @@ public class StatsController {
                                        @RequestParam String end,
                                        @RequestParam(required = false) List<String> uris,
                                        @RequestParam(defaultValue = "false") Boolean unique) {
+        log.debug("controller: запрос на выборку статистики start: {}, end: {}, uris {}, unique {}", start, end, uris, unique);
         LocalDateTime startDate = LocalDateTime.parse(
                 URLDecoder.decode(start, StandardCharsets.UTF_8), DATE_TIME_FORMATTER);
         LocalDateTime endDate = LocalDateTime.parse(
                 URLDecoder.decode(end, StandardCharsets.UTF_8), DATE_TIME_FORMATTER);
+
+        if (startDate.isAfter(endDate)) {
+            throw new ValidationException("Дата начала выборки должна быть до даты конца");
+        }
 
         List<ViewStats> stats = service.getStats(startDate, endDate, uris, unique);
         return stats.stream()
